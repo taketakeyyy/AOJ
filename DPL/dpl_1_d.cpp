10c78202b0415e843ad2f563e8d748cb722b4ab3
taketakeyyy <taketakeyyy@gmail.com>
2021-10-16 02:11:33 +0900

    solved

---
+++ DPL/dpl_1_d.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES  // M_PI等のフラグ
+#define MOD 1000000007
+#define COUNTOF(array) (sizeof(array)/sizeof(array[0]))
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define intceil(a,b) ((a+(b-1))/b)
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long,long>;
+const long long INF = LONG_LONG_MAX - 1001001001001001;
+void chmax(int& x, int y) { x = max(x,y); }
+void chmin(int& x, int y) { x = min(x,y); }
+
+
+// 最長増加部分列(LIS)
+ll LIS(const vector<ll> &A) {
+    ll N = (ll)A.size();
+    vector<ll> dp(N, INF);
+    for(ll i=0; i<N; i++) {
+        auto it = lower_bound(dp.begin(), dp.end(), A[i]);
+        *it = A[i];
+    }
+
+    return lower_bound(dp.begin(), dp.end(), INF) - dp.begin();
+}
+
+void solve() {
+    ll N; cin >> N;
+    vector<ll> A(N);
+    for(int i=0; i<N; i++) cin >> A[i];
+
+    cout << LIS(A) << endl;
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
